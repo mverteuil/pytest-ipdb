6580b689450787b8891be18c69a82a9c5d384c76
Matthew de Veteuil <onceuponajooks@gmail.com>
2013-03-20 11:22:30 -0400

    Updated setup file with description, etc

--- setup.py
+++ setup.py
@@ -2,12 +2,29 @@ from setuptools import setup
 
 setup(
     name="pytest-ipdb",
-    packages = ['pytestipdb'],
-
+    packages=["pytestipdb"],
+    version="0.1-prerelease",
+    description="A py.test plug-in to enable drop to ipdb debugger on test failure.",
+    author="Matthew de Verteuil",
+    author_email="onceuponajooks@gmail.com",
+    url="https://github.com/mverteuil/pytest-ipdb",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Email",
+        "Topic :: Software Development :: Debuggers",
+        "Topic :: Software Development :: Testing",
+    ],
     # the following makes a plugin available to py.test
     entry_points = {
-        'pytest11': [
-            'pytestipdb = pytestipdb.ptipdb',
+        "pytest11": [
+            "pytestipdb = pytestipdb.ptipdb",
         ]
     },
 )
